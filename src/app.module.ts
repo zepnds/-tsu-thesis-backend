3e913ecafa170027331dc60b61928774c8b4371a
zepnds <zepnds@gmail.com>
2026-04-20 09:12:13 +0800

    scan qr

--- src/app.module.ts
+++ src/app.module.ts
@@ -33,7 +33,12 @@ import { MailingModule } from './library/mailing/mailing.module';
           autoLoadEntities: true,
           synchronize: false, // We use existing DB, don't sync in prod
           logging: true,
-          ssl: false,
+          ssl: true,
+          extra: {
+            ssl: {
+              rejectUnauthorized: false,
+            },
+          },
         };
       },
       inject: [ConfigService],
